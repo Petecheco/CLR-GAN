a824a9e27b038f3545e78bad9b98684a4ad53739
Zeki T. Luan <s1324151534@outlook.com>
2024-07-12 01:01:17 +0800

    Web V2
    -Remove External CSS
    +More Content

--- web/js/index.js
+++ web/js/index.js
@@ -9,13 +9,9 @@ setTimeout(() => window.scrollTo(0,0), 150);
 var scrollFunc = function (e) {
     e = e || window.event;
 
-    if (e.wheelDelta) {
-        console.log("IE、Opera、Safari、Chrome");
-        console.log(e.wheelDelta);
+    if (e.wheelDelta) { // Chrominum
         scrollEvent = e.wheelDelta;
-    } else if (e.detail) {
-        console.log("Firefox");
-        console.log(e.detail);
+    } else if (e.detail) { // Gecko
         scrollEvent = e.detail * -1;
     }
     if (bCanScroll) {
@@ -50,7 +46,56 @@ function scrollUp() {
     }
 }
 
+let cInfos = document.getElementsByClassName("c-p2-info");
+
+cInfos[0].addEventListener("mouseover", () => {
+    bCanScroll = false;
+})
+
+cInfos[1].addEventListener("mouseover", () => {
+    bCanScroll = false;
+})
+
+cInfos[0].addEventListener("mouseout", () => {
+    bCanScroll = true;
+})
+
+cInfos[1].addEventListener("mouseout", () => {
+    bCanScroll = true;
+})
+
 if (document.addEventListener) {
     document.addEventListener('DOMMouseScroll', scrollFunc, false);
 }//W3C
 window.onmousewheel = document.onmousewheel = scrollFunc;
+
+let cBtns = document.getElementsByClassName("c-p2-choose-box")[0].children;
+let nowChoose = 0;
+
+cBtns[0].addEventListener('click', () => {
+    if (nowChoose == 1) {
+        nowChoose = 0;
+        cBtns[0].setAttribute('class', 'c-p2-choose-item-choosed');
+        showInfo(0);
+        cBtns[1].setAttribute('class', 'c-p2-choose-item');
+        hideInfo(1);
+    }
+})
+
+cBtns[1].addEventListener('click', () => {
+    if (nowChoose == 0) {
+        nowChoose = 1;
+        cBtns[1].setAttribute('class', 'c-p2-choose-item-choosed');
+        showInfo(1);
+        cBtns[0].setAttribute('class', 'c-p2-choose-item');
+        hideInfo(0);
+    }
+})
+
+function showInfo(i) {
+    cInfos[i].style = "opacity: 1;";
+}
+
+function hideInfo(i) {
+    cInfos[i].style = "opacity: 0; max-height: 0px; margin: 0px; padding: 0px;";
+}
